b98708aac6d94e889669d90981d53aae232d564d
ManuelPiris <bow6969@gmail.com>
2022-05-10 20:51:12 +0200

    Reto8

---
+++ POO/contactTest.js
@@ -0,0 +1,11 @@
+class contacts{
+    constructor(nombre,apellidos,edad,pais){
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+        this.pais = pais;
+    }
+} 
+ 
+let datos = new contacts ("Manuel", "Piris", 34, "España");
+console.log(datos);
